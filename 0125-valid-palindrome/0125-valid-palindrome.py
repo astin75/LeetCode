746190b1f1c32586e45dfd5873c955f191272f6e
astin75 <51067085+astin75@users.noreply.github.com>
2023-03-01 23:56:21 +0900

    Time: 51 ms (58.28%), Space: 15.4 MB (28.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,17 +9,13 @@ class Solution:
             if s.isalpha():
                 return True
         else:
-            new_list = ""
+            if len(s) == 1:
+                return True
+
+            r_s = s[::-1]
             for i in range(len(s)):
                 a = s[i]
-                new_list+=a.lower()
-            if len(new_list) == 1:
-                return True
-            idx = 1
-            for i in range(len(new_list)):
-                a = new_list[i]
-                b = new_list[-idx]
-                idx+=1
+                b = r_s[i]
 
                 if a.lower() != b.lower():
                     return False
