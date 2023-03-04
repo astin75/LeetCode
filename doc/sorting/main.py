b1e47816edc58fa9c467a1a4210d5805149556b5
astin75 <astin75@gmail.com>
2023-03-04 19:46:58 +0900

    sorting

---
+++ doc/sorting/main.py
@@ -0,0 +1,28 @@
+import random
+value_list = list(range(10))
+random.shuffle(value_list)
+print(value_list)
+
+def selection_sort(arr):
+    for idx, i in enumerate(arr):
+        min_idx = idx
+        for jdx in range(idx+1, len(arr)):
+            if arr[jdx] < arr[min_idx]:
+                min_idx = jdx
+        tmp = arr[idx]
+        arr[idx] = arr[min_idx]
+        arr[min_idx] = tmp
+    return arr
+
+def bubble_sort(arr):
+    for idx in range(len(arr)):
+        for jdx in range(0, len(arr)-1-idx):
+            print(jdx, 2)
+            if arr[jdx] > arr[jdx+1]:
+                tmp = arr[jdx]
+                arr[jdx] = arr[jdx+1]
+                arr[jdx+1] = tmp
+    return arr
+
+result = bubble_sort(value_list)
+print(result)
