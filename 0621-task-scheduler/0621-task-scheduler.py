e4eb2390bafec29ee57633c87c365072434e1fc6
astin75 <51067085+astin75@users.noreply.github.com>
2023-03-16 11:34:53 +0900

    Time: 384 ms (92.41%), Space: 14.3 MB (55.88%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,12 +1,14 @@
 from collections import Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        n_place = n +1 # a__ A __
         task_count = list(Counter(tasks).values())
-        max_count = max(task_count) 
-        count_max_task = task_count.count(max_count) # A and B
-        count_idle = max_count -1
-        place = n + 1
-        return max(len(tasks), (count_idle * place + count_max_task))
+        max_count = max(task_count)
+        max_count_task = task_count.count(max_count)
+        count_idle = max_count - 1
+        total_count = count_idle * n_place + max_count_task
+        return max(len(tasks), total_count)
+        
         
         
         
